3e9dce94731b81443244fa92ca0949afeb8ec496
lenacgs <lenacgs@gmail.com>
2017-10-19 01:17:18 +0100

    Still wrong

--- pl1_ex5.m
+++ pl1_ex5.m
@@ -1,16 +1,15 @@
-%esta solu??o n?o funciona.  quando se combinam os simbolos 2 a 2
-%eventualmente vao haver simbolos repetidos, nao pode acontecer
-%nao consegui ainda descobrir como vou resolver isto
-
+%como o alfabeto de onde agrupamos os s?mbolos j? n?o tem simbolos
+%repetidos, entao o novo alfabeto n?o vai ter simbolos repetidos
 
 function pl1_ex5(fonte)
-alfabeto = pl1_ex3(fonte);
+
+alfabeto = [1 2 3 4 5 6 7 8 9];
+%alfabeto = pl1_ex3(fonte);
 %agrupar simbolos do alfabeto 2 a 2:
 %percorrer a matriz do alfabeto 2 vezes, e concatenar numa matriz de
 % strings os s?mbolos 2 a 2.
 m = 1;
 dim = numel(alfabeto)^2;
-disp(dim);
 newalphabet = strings(dim, 1);
 
 for i = 1:numel(alfabeto)
@@ -21,17 +20,21 @@ for i = 1:numel(alfabeto)
         m = m + 1;
     end
 end
-disp(newalphabet);
-
-f = readfonte(fonte);
+f = fonte;
+%f = readfonte(fonte);
 %tornar a fonte numa string
-f = mat2str(f);
+f2 = strjoin(f);
+f3 = f3(~sspace(f3));
 %percorrer a string e ver qual o numero de ocorr?ncias de cada s?mbolo do
 %novo alfabeto
 freq = zeros(numel(newalphabet), 1);
+freq = count(f3, newalphabet);
+disp(freq);
 for n = 1:numel(newalphabet)
-    freq(n) = count(f, newalphabet(n));
+    freq(n) = count(f2, newalphabet(n));
 end
+
+disp(freq);
 
 %numero medio de bits por s?mbolo
 prob = freq./sum(freq);
