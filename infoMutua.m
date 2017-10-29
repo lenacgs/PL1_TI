9438c8b4efa452bce66409ebcfdc864cc88123be
lenacgs <lenacgs@gmail.com>
2017-10-29 13:13:52 +0000

    6b a funcionar

---
+++ infoMutua.m
@@ -0,0 +1,29 @@
+function [infoMutua] = infoMutua(alf, query, target)
+
+freqs = zeros(numel(alf), numel(alf));
+step = numel(query)/4;
+
+for i = 1:step:numel(target)
+    for j = 1:numel(query)
+        indexq = round((query(j) + 1) / 0.0078);
+        indext = round((target(j) + 1) / 0.0078);
+        if (indext == 0), indext = 1; end
+        freqs(indext, indexq) = freqs(indext, indexq) +1;
+    end
+end
+
+pxy = freqs ./ numel(freqs);
+pxy = pxy(pxy~=0);
+fx = histcounts(target);
+px = fx ./ numel(target);
+
+fy = histcounts(query);
+py = fy ./ numel(query);
+a = px.*py;
+
+b = pxy ./ a;
+infoMutua = pxy .* (log(b));
+
+
+end
+
--- pl1_ex6Mada.m
+++ pl1_ex6Mada.m
@@ -4,57 +4,22 @@ function pl1_ex6Mada
 [target01, ~, ~, ~] = infoSom('target01 - repeat.wav');
 [target02, ~, ~, ~] = infoSom('target02 - repeatNoise.wav');
 
-%os 3 alfabetos sao iguais
-%matriz alf x alf
-freqs = zeros(numel(alf), numel(alf));
-%probs x target
-%probs y query
+i1 = infoMutua(alf, query, target01);
+subplot(2,1,1);
+plot(i1);
+ylim([0, 8]);
+ylabel('mutual information');
+xlabel('time');
+title('mutual information: target01 & query');
 
-step = numel(query)/4;
+i2 = infoMutua(alf, query, target02);
+subplot(2,1,2);
+plot(i2);
+ylim([0, 0.3]);
+ylabel('mutual information');
+xlabel('time');
+title('mutual information: target02 & query');
 
-for i = 1:step:numel(target01)
-    for j = 1:numel(query)
-        indexq = round((query(j) + 1) / 0.0078);
-        indext01 = round((target01(j) + 1) / 0.0078);
-        freqs(indext01, indexq) = freqs(indext01, indexq) +1;
-    end
-end
-
-pxy = freqs ./ numel(freqs);
-pxy = pxy(pxy~=0);
-fx = histcounts(target01);
-px = fx ./ numel(target01);
-
-fy = histcounts(query);
-py = fy ./ numel(query);
-a = px.*py;
-
-b = pxy ./ a;
-infoMutua = pxy .* (log(b));
-disp('target01');
-%plot(infoMutua);
-
-freqs2 = zeros(numel(alf), numel(alf));
-
-for i = 1:step:numel(target02)
-    for j =1:numel(query)
-        indexq2 = round((query(j) +1) / 0.0078);
-        
-        indext02 = round((target02(j) + 1) / 0.0078);
-        freqs2(indext02, indexq2) = freqs2(indext02, indexq2) +1;
-    end
-end
-pxy2 = freqs2 ./ numel(freqs2);
-pxy2 = pxy2(pxy2~=0);
-fx2 = histcounts(target02);
-px2 = fx2 ./ numel(target02);
-
-c = px2 .* py;
-d = pxy2 ./ c;
-
-infoMutua2 = pxy2.* log(d);
-disp('target02');
-plot(infoMutua2);
 end
 
 
