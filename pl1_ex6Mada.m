30371c44a3828864a86285ae18fa2a002ec4cda3
lenacgs <lenacgs@gmail.com>
2017-10-29 12:31:11 +0000

    ex6b feito (pl1_ex6Mada) so falta organizar e testar melhor

--- pl1_ex6Mada.m
+++ pl1_ex6Mada.m
@@ -1,29 +1,60 @@
 function pl1_ex6Mada
 
-[query, freq, nBits, alf] = infoSom('guitarSolo.wav');
-[target01, freq1, nBits1, alf1] = infoSom('target01 - repeat.wav');
-[target02, freq2, nBits2, alf2] = infoSom('target02 - repeatNoise.wav');
+[query, ~, ~, alf] = infoSom('guitarSolo.wav');
+[target01, ~, ~, ~] = infoSom('target01 - repeat.wav');
+[target02, ~, ~, ~] = infoSom('target02 - repeatNoise.wav');
 
 %os 3 alfabetos sao iguais
 %matriz alf x alf
-probs = zeros(numel(alf), numel(alf));
-%probs x ? query
-%probs y ? target
-
-disp(numel(alf));
+freqs = zeros(numel(alf), numel(alf));
+%probs x target
+%probs y query
 
 step = numel(query)/4;
-for i=1:step:numel(query)
-    for j=1:numel(target01)
-        aux1 = round(abs(query(j))*100);
-        aux2 = round(abs(target01(j))*100);
-        disp (aux1);
-        disp(aux2);
-        probs(aux1, aux2) = probs(aux1, aux2) + 1;
+
+for i = 1:step:numel(target01)
+    for j = 1:numel(query)
+        indexq = round((query(j) + 1) / 0.0078);
+        indext01 = round((target01(j) + 1) / 0.0078);
+        freqs(indext01, indexq) = freqs(indext01, indexq) +1;
     end
 end
 
+pxy = freqs ./ numel(freqs);
+pxy = pxy(pxy~=0);
+fx = histcounts(target01);
+px = fx ./ numel(target01);
 
+fy = histcounts(query);
+py = fy ./ numel(query);
+a = px.*py;
 
+b = pxy ./ a;
+infoMutua = pxy .* (log(b));
+disp('target01');
+%plot(infoMutua);
+
+freqs2 = zeros(numel(alf), numel(alf));
+
+for i = 1:step:numel(target02)
+    for j =1:numel(query)
+        indexq2 = round((query(j) +1) / 0.0078);
+        
+        indext02 = round((target02(j) + 1) / 0.0078);
+        freqs2(indext02, indexq2) = freqs2(indext02, indexq2) +1;
+    end
 end
+pxy2 = freqs2 ./ numel(freqs2);
+pxy2 = pxy2(pxy2~=0);
+fx2 = histcounts(target02);
+px2 = fx2 ./ numel(target02);
+
+c = px2 .* py;
+d = pxy2 ./ c;
+
+infoMutua2 = pxy2.* log(d);
+disp('target02');
+plot(infoMutua2);
+end
+
 
